00522075abd7bae48de8cbbd6497669422d4bc71
GT15725128838 <1946326866@qq.com>
2023-06-10 22:10:18 +0800

    初始化

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# My-first-repository
+# The-Oxford-IIIT-Pet-Dataset动物数据集分类识别
+
+## 1. 数据集获取
+    下载Oxford-IIIT Pet Dataset<https://www.robots.ox.ac.uk/~vgg/data/pets/>
+## 2.图像预处理
+## 3.划分数据集
+## 4.构建模型
+## 5.模型训练
+## 6.模型预测
+  
+    
---
+++ data_pre_processing.py
@@ -0,0 +1,10 @@
+import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
